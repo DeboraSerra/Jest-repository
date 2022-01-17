1e58f0115d1173e6a03bd72027d7fc98f05e31c7
Débora Serra <debora.r.serra@gmail.com>
2022-01-17 15:25:17 -0300

    Adiciona requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -28,7 +28,14 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
+    expect(typeof productDetails).toBe('function');
+    expect(productDetails('Alcool', 'Máscara').length).toBeTruthy();
+    expect(productDetails('Alcool', 'Máscara').length).toBe(2);
+    expect(typeof productDetails('Alcool', 'Máscara')[0]).toBe('object');
+    expect(typeof productDetails('Alcool', 'Máscara')[1]).toBe('object');
+    expect(productDetails('Alcool', 'Máscara')[0] !== productDetails('Alcool', 'Máscara')[1]).toBe(true);
+    expect(productDetails('Alcool', 'Máscara')[0].details.productId).toContain('Alcool123');
+    expect(productDetails('Alcool', 'Máscara')[1].details.productId).toContain('Máscara123')
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se productDetails é uma função.
     // Teste se o retorno da função é um array.
